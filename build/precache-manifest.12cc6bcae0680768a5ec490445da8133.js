5fb7c98710062a6519933e2b9da6b920df70d664
Obaid Ur-Rahmaan <oxu711@cs.bham.ac.uk>
2020-07-08 19:49:07 +0100

    Added build directory

---
+++ build/precache-manifest.12cc6bcae0680768a5ec490445da8133.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06f5e4ae94fc9f6a03596fdc1dc03c82",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bcca4a0c1be2dbaaee76",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d347199b6eb476174db2",
+    "url": "/static/js/2.5cc144e3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5cc144e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bcca4a0c1be2dbaaee76",
+    "url": "/static/js/main.e1db134d.chunk.js"
+  },
+  {
+    "revision": "652af80ddc61580c40a6",
+    "url": "/static/js/runtime-main.ea45be16.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
